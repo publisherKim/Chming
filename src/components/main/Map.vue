b7a5807f7882d629859c967c0e4203d01f41d701
incorrectDeveloper <hdpgpp@gmail.com>
2017-07-31 16:09:57 +0900

    eslint test

---
+++ src/components/main/Map.vue
@@ -0,0 +1,35 @@
+<template>
+  <h2>{{ msg }}</h2>
+</template>
+
+<script>
+export default {
+  name: 'Map',
+  data() {
+    return {
+      msg: 'Welcome Map',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
