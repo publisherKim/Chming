0d394821e999730d0d5a8359a8999536a8a2a5aa
incorrectDeveloper <hdpgpp@gmail.com>
2017-08-04 13:02:32 +0900

    라우트분리 css수정

--- src/components/user/Join.vue
+++ src/components/user/Join.vue
@@ -4,45 +4,16 @@
       h3.title 회원가입
       form.join-user_form
         p  
-          input.name(
-            type="text" 
-            placeholder="이름" 
-            aria-label="이름"
-          )
+          input.name(type="text" placeholder="이름" aria-label="이름")
         p  
-          input.email(
-            type="email" 
-            placeholder="이메일" 
-            aria-label="이메일"
-          )
+          input.email(type="email" placeholder="이메일" aria-label="이메일")
         p  
-          input.password(
-            type="password" 
-            placeholder="비밀번호" 
-            aria-label="비밀번호"
-          )
+          input.password(type="password" placeholder="비밀번호" aria-label="비밀번호")
         p 
           label(class="birth" for="birth") 생년월일
-          input.year(
-            id="birth"
-            type="number" 
-            placeholder="1993" 
-            min="1900" 
-            :max="maxYear" 
-            aria-label="생년"
-          )
-          input.month(
-            type="number" 
-            placeholder="07" 
-            min="1" max="12" 
-            aria-label="월"
-          )
-          input.day(
-            type="number" 
-            placeholder="07" 
-            min="1" max="31" 
-            aria-label="일"
-          )
+          input.year(id="birth" type="number" placeholder="1993" min="1900" :max="maxYear" aria-label="생년")
+          input.month(type="number" placeholder="07" min="1" max="12" aria-label="월")
+          input.day(type="number" placeholder="07" min="1" max="31" aria-label="일")
         p
           input#man(type="radio" name="gender" checked)
           label(for="man") 남자
@@ -50,28 +21,20 @@
           label(for="woman") 여자
         div
           h4.title 관심사 설정 
-            button(
-              @click=""
-              type="button"
-            ) 설정
+            button(@click="changeRoute('user_join_interest')" type="button") 설정
           ul.interest-list
             li
               img(src="" alt="관심사1")
         div
           h4.title 지역 선택 
-            button(
-              @click=""
-              type="button"
-            ) 설정
+            button(@click="" type="button") 설정
           p.address 경기도 성남시 분당구 정자동 11-2
-        button(
-          class="join-confirm" 
-          type="submit") 완료
-    user-interest(@click="toggleMenu")
+        button(class="join-confirm" type="submit") 완료
+    router-view
 </template>
 
 <script>
-  import UserInterest from '@/components/user/Interest';
+  import UserInterest from '@/components/common/Interest';
 
   export default {
     data() {  
@@ -86,6 +49,12 @@
     methods: {
       toggleMenu() {
         console.log(11);
+      },
+      changeRoute(name) {
+        console.log(name);
+        this.$router.push({
+          name: name
+        });
       }
     }
   };
@@ -99,17 +68,17 @@
     padding-left: 1.5rem
       
   .join-user-wrap
+    display: block
+    height: 100vh
     padding-top: 2rem
     +span(width 100% nest)
     padding: 0 1.5rem
     background: #e1e1e1
     border: 1px solid #ccc
-    min-height: 100vh
   .active 
     display: block
   h3.title
-    border-bottom: solid 1px #b1b1b1
-    line-height: 3rem
+    +subTitle
   .join-user_form
     .address
       margin-top: 1rem
@@ -131,7 +100,7 @@
     margin-top: 1.5rem
   .join-confirm
     display: block
-    margin: 10px auto      
+    margin: 1rem auto      
 
 
 </style>
--- src/components/user/Login.vue
+++ src/components/user/Login.vue
@@ -1,52 +1,39 @@
 <template lang="pug">
   .join-user-wrap
-    h3 로그인
+    h3.title 로그인
     form.join-user_form
       p  
-        input.email(
-          type="email" 
-          placeholder="이메일" 
-          aria-label="이메일"
-        )
+        input.email(type="email" placeholder="이메일" aria-label="이메일")
       p  
-        input.password(
-          type="password" 
-          placeholder="비밀번호" 
-          aria-label="비밀번호"
-        )
+        input.password(type="password" placeholder="비밀번호" aria-label="비밀번호")
     ul.join-user_find-list
       li
         a 아이디 / 비밀번호 찾기
       li
-        a(@click="join") 회원가입
+        a(@click="chageRoute") 회원가입
     button.button-login(@click="login") 로그인
     button.button-close(@click="loginClose") 닫기
-    user-interest
 </template>
 
 <script>
-import UserInterest from '@/components/user/Interest';
 
-export default {
-  components: {
-    UserInterest
-  },
-  methods: {
-    loginClose() {
-      this.$router.push({
-        name: 'main'
-      });
-    },
-    login() {
-      console.log('로그인 성공시 : true, 로그인 실패시 : 실패 메시지');
-    },
-    join() {
-      this.$router.push({
-        name: 'user_join'
-      });
+  export default {
+    methods: {
+      loginClose() {
+        this.$router.push({
+          name: 'main'
+        });
+      },
+      login() {
+        console.log('로그인 성공시 : true, 로그인 실패시 : 실패 메시지');
+      },
+      chageRoute() {
+        this.$router.push({
+          name: 'user_join'
+        });
+      }
     }
-  }
-};
+  };
 </script>
 
 <style lang="sass" scoped>
@@ -60,11 +47,10 @@ export default {
     padding: 3rem
     .active 
       display: block
-    h3
-      title 1.4rem
-      border-bottom: solid 1px #b1b1b1
-      line-height: 3rem
+    h3.title
+      +subTitle
     p
+      margin-top: 1.5rem
       input 
         width: 100%
         line-height: 2rem
@@ -81,6 +67,9 @@ export default {
       right: 3rem
     .button-login
 
-    
+  .join-user_find-list
+    margin-top: 1.5rem
+    li
+      line-height: 2rem
 
 </style>
