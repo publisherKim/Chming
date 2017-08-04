6cfa32a4b3a4d6b88ed4b7e65378cb6b9171ec6e
LeeDaeHo <dleogh29@gmail.com>
2017-08-04 22:14:07 +0900

    필터 기능 수정

--- src/components/filter/InterestFilter.vue
+++ src/components/filter/InterestFilter.vue
@@ -2,22 +2,19 @@
   .interest-container
     h2.interest_title 관심사 필터
     .interest_header-wrap
-      button(type="button" @click="") 취소
-      button(type="button" @click="") 필터 적용
+      button.header_cancel-button(type="button" @click="") 취소
+      button.header_apply-button(type="button" @click="") 필터 적용
     .interest_content-wrap
-      .interest_list-wrap
-        h2 운동/스포츠
-        ul
-          li 
-            button 자전거
-          li 
-            button 배드민턴
-          li 
-            button 볼링
+      interest
 </template>
 
 <script>
+import Interest from '../common/Interest';
+
 export default {
+  components: {
+    Interest,
+  },
   data() {
     return {
 
@@ -29,29 +26,43 @@ export default {
 <style lang="sass" scoped>
   @import "~chming"
 
-  +mobile
+  $interest-header-height: 30px
+
+  button
+    background: none
+    font-size: 1.2rem
+  .interest-container
+    width: 100%
+    z-index: 10
+  .interest_title
+    +a11y-hidden
+  .interest_header-wrap
+    background: rgb(248, 248, 248)
+    display: block
+    height: $interest-header-height
+    +clearfix
     button
-      background: none
-      font-size: 1.2rem
-    .interest-container
-      z-index: 10
-    .interest_title
-      +a11y-hidden
-    .interest_header-wrap
-      background: rgb(248, 248, 248)
-      button
-        border: 0
-    .interest_content-wrap
-      background: #fff
-    .interest_list-wrap
-      +clearfix
-      h2
-        margin: 0
-      li
-        float: left
-        margin-right: gutter()
-      button
-        border: 1px solid #000
-        border-radius: 1rem
-        padding: 3px 5px
+      border: 0
+      font-weight: bold
+      position: relative
+      +align-vertical-middle
+    .header_cancel-button
+      float: left
+      margin-left: 1rem
+    .header_apply-button
+      float: right
+      margin-right: 1rem
+  .interest_content-wrap
+    background: #fff
+  .interest_list-wrap
+    +clearfix
+    h2
+      margin: 0
+    li
+      float: left
+      margin-right: gutter()
+    button
+      border: 1px solid #000
+      border-radius: 1rem
+      padding: 3px 5px
 </style>
