d2b7f654fe61236ab9a981fdc35b302fbf877fb3
LeeDaeHo <dleogh29@gmail.com>
2017-08-05 10:46:38 +0900

    취소 버튼 공통 컴포넌트 추가

---
+++ src/components/common/CancelButton.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+  button.cancel_button(@click="changeRoute")
+    i.fa.fa-times(aria-hidden="true")
+</template>
+
+<script>
+export default {
+  props: {
+    route: {
+      type: String,
+      required: true
+    },
+  },
+  methods: {
+    changeRoute() {
+      this.$router.push({name: this.route});
+    },
+  },
+};
+
+</script>
+
+<style lang="sass" scoped>
+  
+</style>
