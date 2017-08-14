ce4af9cd38afa19da52d27981379038e6659622c
incorrectDeveloper <hdpgpp@gmail.com>
2017-08-14 11:34:06 +0900

    모임개설 완료

--- src/components/group/Create.vue
+++ src/components/group/Create.vue
@@ -2,32 +2,82 @@
   .group-create-container
     h3.title 모임 개설
     form.group-create_form
-      input.form_name(v-model="group.name" @blur="checkEmpty('name')" ref="name" type="text" placeholder="모임 이름을 작성해주세요." aria-label="모임 이름")
-      message-box(
-        v-if="isEmptyGroupName" :classList="['fa-check-circle-o', 'warning']" message="그룹명을 입력해주세요."
+      input.form_name(
+        v-model="group.name" 
+        @blur="checkEmpty('name')" 
+        ref="name" 
+        type="text" 
+        placeholder="모임 이름을 작성해주세요." 
+        aria-label="모임 이름"
       )
-      textarea.form_description(v-model="group.description" placeholder="어떤 모임인지 설명해주세요." aria-label="모임 설명")
+      message-box(
+        v-if="isEmptyGroupName"
+        :classList="['fa-check-circle-o', 'warning']"
+        message="그룹명을 입력해주세요."
+      )
+      textarea.form_description(
+        v-model="group.description"
+        @blur="checkEmpty('description')" 
+        ref="description" 
+        placeholder="어떤 모임인지 설명해주세요." 
+        aria-label="모임 설명"
+      )
+      message-box(
+        v-if="isEmptyGroupDescription"
+        :classList="['fa-check-circle-o', 'warning']"
+        message="그룹설명을 입력해주세요."
+      )
       .form_file-upload-wrap
-        input#upload(@change="fileUpload" type="file")
-        label.file-upload_label(for="upload") 모임 대표 사진
-          i.fa.fa-picture-o(aria-hidden="true")
-          img.label_group-img(v-if="group.introduceImg" :src="uploadSrc")
+        input#upload(
+          @change="fileUpload" 
+          type="file"
+        )
+        label.file-upload_label(
+          for="upload"
+          ref="fileUpload" 
+          @keyup.enter="$refs.fileUpload.click()" 
+          tabindex="0" 
+          role="button"
+        ) 모임 대표 사진
+          .fa.fa-picture-o(aria-hidden="true")
+          img.label_group-img(
+            v-if="group.image" 
+            :src="uploadSrc"
+          )
       .form_location-wrap
         button.location_button(
           @click="changeRoute({name: 'group_create_location', params: {prev: 'group_create'}})"
+          @blur="checkEmpty('address')"
+          ref="address" 
           type="button"
         ) 지역선택
           i.fa.fa-map-marker(aria-hidden='true')
-        p.location-address {{group.address}}
+        p.location-address {{group.address}}      
+      message-box(
+        v-if="isEmptyGroupAddrss"
+        :classList="['fa-check-circle-o', 'warning']"
+        message="지역을 선택해주세요"
+      )
+  
       .form_hobby-wrap
         button.hobby_button(
           @click="changeRoute({name: 'group_create_hobby', params: {prev: 'group_create'}})"
+          @blur="checkEmpty('hobby')"
           type="button"
         ) 관심사 설정
-          i.fa.fa-cog(aria-hidden='true')
-        ul.hobby-list
-          li {{group.hobby}}
-      button.form_confirm(@click="changeRoute('main')" type="submit") 완료
+          i.fa.fa-cog(aria-hidden='true') 
+        ul.hobby-list(v-if="group.hobby && group.hobby.length !== 0")
+          li.list_item(v-for="hobby in group.hobby")
+            img(src="" :alt="hobby")
+      message-box(
+        v-if="isEmptyGroupHobby"
+        :classList="['fa-check-circle-o', 'warning']"
+        message="관심사를 선택해주세요"
+      ) 
+      button.form_confirm(
+        @click="createGroup" 
+        type="button"
+      ) 완료
     router-view.hobby-container
     back-button(:route={name: 'user_info', params})
 </template>
@@ -37,39 +87,50 @@
   import BackButton from '@/components/common/BackButton';
   import MessageBox from '@/components/common/MessageBox';
   import Vue from 'vue';
-  import { mapGetters } from 'vuex';
+  import { mapMutations } from 'vuex';
 
   export default {
     components: {
       GroupHeader,
       BackButton,
-    },
-    computed: {
-      isEmptyGroupName() {
-        return this.group.name === '';
-      },
+      MessageBox
     },
     data(){
       return {
         uploadSrc: '',
         group: {
           name: null,
-          description: '',
-          introduceImg: null,
-          address: 'test',
+          description: null,
+          image: '',
+          address: null,
           lat: '',
           lng: '',
-          hobby: 'test1',
+          hobby: null,
         }
       };
     },
+    computed: {
+      isEmptyGroupName() {
+        return this.group.name === '';
+      },
+      isEmptyGroupDescription() {
+        return this.group.description === '';
+      },
+      isEmptyGroupAddrss() {
+        return this.group.address === '';
+      },
+      isEmptyGroupHobby(){
+        let hobby = this.group.hobby;
+        return hobby && hobby.length === 0;
+      }
+    },    
     methods: {
       changeRoute(route) {
         this.$router.push(route);
       },
       fileUpload(e) {
         let file = e.target.files[0];
-        this.group.introduceImg = file;
+        this.group.image = file;
         let reader = new FileReader();
         reader.readAsDataURL(file);
         reader.onload = f => {
@@ -77,7 +138,6 @@
         };
       },
       checkEmpty(field) {
-        console.log(field);
         let group = this.group;
         (field === 'hobby' && group[field] === null) && (group[field] = []);
         group[field] === null && (group[field] = '');
@@ -85,12 +145,46 @@
       groupValidate(field) {
         let refs = this.$refs;
         let group = this.group;
+
         if(this.isEmptyGroupName || !group.name) {
           this.checkEmpty('name');
           refs.name.focus();
           return false;
         }
+        if(this.isEmptyGroupDescription || !group.description) {
+          this.checkEmpty('description');
+          refs.description.focus();
+          return false;
+        }
+        if(this.isEmptyGroupAddrss || !group.address){
+          this.checkEmpty('address');
+          refs.address().focus();
+          return false;
+        }
+        if(this.isEmptyGroupHobby || !group.hobby){
+          this.checkEmpty('hobby');
+          refs.hobby().focus();
+          return false;
+        }
+
         return true;
+      },
+      createGroup() {
+        if(!this.groupValidate()) return;
+        let formData = Vue.setFormData(this.group);
+        let token = sessionStorage.getItem('token');
+        console.log(formData);
+        this.$http.post('/group/register/', formData, {headers: {Authorization: `Token ${token}`}}).
+          then(response => {
+            if(response.status === 200) {
+              console.log(response);
+            } else {
+              console.log(response);
+            }
+          })
+          .catch(error => {
+            console.log('error.response: ', error.response);
+          });
       },
     },
     watch: {
